f5defe32233d11794e7bfbdfffc01d1cfc58b067
liuminwen <84269882+liuminwen@users.noreply.github.com>
2021-05-16 19:34:46 +0800

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -16,9 +16,9 @@ parser = argparse.ArgumentParser(
     description='Predict segmentation result from a given image')
 parser.add_argument('--model', type=str, default='fcn32s_vgg16_voc',
                     help='model name (default: fcn32_vgg16)')
-parser.add_argument('--dataset', type=str, default='pascal_aug', choices=['pascal_voc/pascal_aug/ade20k/citys'],
+parser.add_argument('--dataset', type=str, default='pascal_aug', choices=['pascal_voc,pascal_aug,ade20k,citys'],
                     help='dataset name (default: pascal_voc)')
-parser.add_argument('--save-folder', default='~/.torch/models',
+parser.add_argument('--save-folder', default='./runs/models',
                     help='Directory for saving checkpoint models')
 parser.add_argument('--input-pic', type=str, default='../datasets/voc/VOC2012/JPEGImages/2007_000032.jpg',
                     help='path to the input picture')
